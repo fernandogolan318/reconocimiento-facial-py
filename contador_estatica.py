7beabc8eb80f7c55cd15484876a2283c4daca3e5
Fernando <fernandogl.tij19@utsjr.edu.mx>
2023-07-14 15:53:12 -0600

    Se ordenaron las carpetas para agregar pruebas unitarias

--- contador_estatica.py
+++ contador_estatica.py
@@ -3,7 +3,7 @@ import numpy as np
 
 valorGauss = 3
 valorKernel = 3
-original = cv2.imread('C:/Users/SWIFT 3/source/Phyton/Entrenamiento/Monedas_Contorno/monedas.jpg')
+original = cv2.imread('C:/Users/SWIFT 3/source/Phyton/Entrenamiento/Monedas_Contorno/img/monedas.jpg')
 grises = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
 gauss = cv2.GaussianBlur(grises,(valorGauss,valorKernel),0 )
 
